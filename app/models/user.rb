593b0e7f7fac7798b3eb4ee0562bbf8be4458968
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-08-11 14:10:19 +0000

    [Finish] コメント機能の追

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
-  
+  has_many :post_comments, dependent: :destroy
   has_one_attached :profile_image
   
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
