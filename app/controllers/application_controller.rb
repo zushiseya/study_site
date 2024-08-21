65fa2799fe9512adc81c84e95a7dab47cb26206a
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-08-21 10:41:48 +0000

    管理者機能を実装しよう - 認証機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,32 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top,:about]
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    posts_path
-  end
+  before_action :configure_authentication
   
-  def after_sign_out_path_for(resource)
-    about_homes_path
+  private
+ 
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
+  end
+ 
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
+  end
+ 
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
 
+  # Deviseで許可するパラメータを設定
   protected
 
   def configure_permitted_parameters
+    # サインアップ時に許可するパラメータ
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # アカウント更新時に許可するパラメータ
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
