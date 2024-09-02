c495dd1047480d33378faa1268de14f0d93ad3b7
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-09-02 06:06:49 +0000

    コメントの管理機能の不具合修正

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,24 +1,19 @@
 class Admin::CommentsController < ApplicationController
-  before_action :authenticate_user!
 before_action :authenticate_admin!
   
   def index
     @comments = Comments.all
   end
 
-  def destroy
-    @comment = Comments.find(params[:id])
-    @comment.destroy
-    redirect_to admin_comments_path, notice: 'コメントが削除されました'
-  end 
+def destroy
+  @comment = PostComment.find(params[:id])
+  @comment.destroy
+  redirect_to admin_post_path(@comment.post.id), notice: 'コメントが削除されました'
+end
   
    private
    
    def comments_params
      params.require(:comment).permit(:content)
    end 
-   
-  def authenticate_admin!
-    redirect_to root_path, alert: "管理者権限が必要です。" unless current_user.admin?
-  end
 end 
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
-  
+  layout 'admin'
+
   def index
     @posts = Post.all
   end 
@@ -17,15 +18,9 @@ class Admin::PostsController < ApplicationController
   
   private
   
-  def post_params
-    params.require(:post).permit(:title, :content)
-  end  
-  
   def authenticate_admin!
-    if current_user.nil?
-      redirect_to new_user_session_path, alert: "ログインが必要です。"
-    elsif !current_user.admin?
-      redirect_to root_path, alert: "管理者権限が必要です。"
+    unless admin_signed_in?
+      redirect_to new_admin_session_path, alert: "管理者としてログインが必要です。"
     end
   end
 end
