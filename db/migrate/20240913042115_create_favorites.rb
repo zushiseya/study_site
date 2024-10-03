5fe180f01206399fcda5c87243d77216b33e4ce9
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-10-03 12:27:24 +0000

    デプロイ

--- db/migrate/20240913042115_create_favorites.rb
+++ db/migrate/20240913042115_create_favorites.rb
@@ -2,7 +2,7 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
       t.integer :user_id
-      t.integer :post_imaged_id
+      t.integer :post_id
 
       t.timestamps
     end
